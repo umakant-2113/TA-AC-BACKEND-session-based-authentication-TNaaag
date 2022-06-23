e81f1c7f911a8799e86f401eabdda30a7a5faf06
umakant-2113 <lodhiumakant800@gmail.com>
2022-06-23 18:56:10 +0530

    Done

---
+++ block-BNaaey/demo/app.js
@@ -0,0 +1,62 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const mongoose = require('mongoose');
+let session = require('express-session');
+let mongoStore=require("connect-mongo") (session);
+
+require('dotenv').config();
+// mongoose connected
+mongoose.connect('mongodb://127.0.0.1/login', (err) => {
+  console.log(err ? err : 'mongoose connected');
+});
+// add session
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+// add session
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: new mongoStore({mongooseConnection : mongoose.connection})
+  })
+);
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
